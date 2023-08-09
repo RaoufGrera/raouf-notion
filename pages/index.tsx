21e79db799020ad10826a849ec91e553884d9ad3
raoufgr <raouf.grera@gmail.com>
2023-08-09 22:14:05 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export const getStaticProps = async () => {
     return { props, revalidate: 10 }
   } catch (err) {
     console.error('page error', domain, err)
-
+    //we
     // we don't want to publish the error version of this page, so
     // let next.js know explicitly that incremental SSG failed
     throw err
