a6946d48c1ac0dbd4687d36a18fed2fb3161eabc
raoufgr <raouf.grera@gmail.com>
2024-04-05 01:50:22 +0200

    a

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,7 @@ export const FooterImpl: React.FC = () => {
 
 
       <footer className={styles.footer}>
-        <div className={styles.copyright}>Copyright 2024 {config.author}</div>
+        <div className={styles.copyright}> Copyright 2024 {config.author}</div>
 
         <div className={styles.settings}>
           {hasMounted && (
