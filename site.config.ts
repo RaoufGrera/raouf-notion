069465a0757d34c305e9eac80a79c326f2a69e0d
raoufgr <raouf.grera@gmail.com>
2024-04-13 01:21:47 +0200

    linkedin

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'raoufgr',
   github: 'RaoufGrera',
-  linkedin: 'raoufgr',
+  linkedin: 'raoufgrera',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
